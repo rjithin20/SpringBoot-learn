eb30c7af63f05207f9cd7435b3e9b877f552e337
Jithin <rjithin2001@gmail.com>
2024-10-31 17:55:55 +0530

    step 04-02  Field injection
    Field injection - dependency is injected using reflection

--- learn-spring-framework-02/src/main/java/com/sb/learn/examples/a1/DependencyInjectionLauncherApplication.java
+++ learn-spring-framework-02/src/main/java/com/sb/learn/examples/a1/DependencyInjectionLauncherApplication.java
@@ -2,12 +2,39 @@ package com.sb.learn.examples.a1;
 
 import java.util.Arrays;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Component
+class YourBusinessClass{
+	
+//	Field injection - dependency is injected using reflection
+	@Autowired
+	Dependency1 dependency1;
+	@Autowired
+	Dependency2 dependency2;
+	
+	public String toString() { 
+		return "Using " + dependency1 + " and " + dependency2;
+	} 
+}
+
+@Component
+class Dependency1{
+	
+}
+
+@Component
+class Dependency2{
+	
+}
+
 
 @Configuration
-@ComponentScan("com.sb.learn.examples.a1") // will scan for spring components in that particular package
+@ComponentScan //scan in the current package
 public class DependencyInjectionLauncherApplication {
 
 	public static void main(String[] args) {
@@ -16,6 +43,7 @@ public class DependencyInjectionLauncherApplication {
 				(DependencyInjectionLauncherApplication.class)){
 			Arrays.stream(context.getBeanDefinitionNames())
 			.forEach(System.out::println);
+			System.out.println(context.getBean(YourBusinessClass.class));
 		}
 	}
 }
