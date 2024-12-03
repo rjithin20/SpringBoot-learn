75c764362aef03379f56018d3908b42607c22527
Jithin <rjithin2001@gmail.com>
2024-12-03 12:33:50 +0530

    step 04-03 Constructor injection

--- learn-spring-framework-02/src/main/java/com/sb/learn/examples/a1/DependencyInjectionLauncherApplication.java
+++ learn-spring-framework-02/src/main/java/com/sb/learn/examples/a1/DependencyInjectionLauncherApplication.java
@@ -16,18 +16,28 @@ class YourBusinessClass{
 	
 	Dependency2 dependency2;
 	
-	@Autowired
-	public void setDependency1(Dependency1 dependency1) {
-		System.out.println("Setter Injection - setDependency1");
+	//spring team recommends constructor injection
+	//constructor injection using constructor
+	//@Autowired is not mandatory for constructor injection
+	public YourBusinessClass(Dependency1 dependency1, Dependency2 dependency2) {
+		super();
+		System.out.println("Contructor Injection - YourBusinessClass");
 		this.dependency1 = dependency1;
-	}
-
-	@Autowired
-	public void setDependency2(Dependency2 dependency2) {
-		System.out.println("Setter Injection - setDependency2");
 		this.dependency2 = dependency2;
 	}
 
+	//setter injection using setter methods
+//	@Autowired
+//	public void setDependency1(Dependency1 dependency1) {
+//		System.out.println("Setter Injection - setDependency1");
+//		this.dependency1 = dependency1;
+//	}
+//
+//	@Autowired
+//	public void setDependency2(Dependency2 dependency2) {
+//		System.out.println("Setter Injection - setDependency2");
+//		this.dependency2 = dependency2;
+//	}
 
 	public String toString() { 
 		return "Using " + dependency1 + " and " + dependency2;
@@ -36,12 +46,10 @@ class YourBusinessClass{
 
 @Component
 class Dependency1{
-	
-}
+} 
 
 @Component
 class Dependency2{
-	
 }
 
 
